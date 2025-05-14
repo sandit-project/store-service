2eae5f9f19c3de13c866d45337f1a5b2a9458060
elec1001 <elec1001@gmail.com>
2025-05-14 09:57:33 +0900

    25/05/14

--- src/main/java/com/example/storeservice/dto/StoreResponseDTO.java
+++ src/main/java/com/example/storeservice/dto/StoreResponseDTO.java
@@ -18,6 +18,6 @@ public class StoreResponseDTO {
     private String  storePostcode;
     private Double  storeLatitude;
     private Double  storeLongitude;
-    private OrderStatus storeStatus;
+    private String storeStatus;
     private LocalDateTime storeCreatedDate;
 }
